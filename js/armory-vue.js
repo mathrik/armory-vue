084fed9c7b913383bf562982306a0ed0df59312d
Curtis Mortensen <curtins@gmail.com>
2018-11-06 10:37:56 -0700

    Added navigation with hooks for displaying various character information

--- js/armory-vue.js
+++ js/armory-vue.js
@@ -1,15 +1,21 @@
 var landingpage = new Vue({
   el: '.app-container',
   data: {
+  	achievesVisible: false,
   	apiData: {},
     avatar: 'https://render-us.worldofwarcraft.com/character/wyrmrest-accord/157/137192861-avatar.jpg',
+    apiDataLocation: 'https://us.api.battle.net/wow',
+    battlePetsVisible: false,
     character: 'Daokou',
     characterName: 'Daokou',
     characterRace: 'Pandaren',
     characterClass: 'Warrior',
     charclasses: {},
     charraces: {},
+    mountsVisible: false,
+    profileVisible: false,
     realm: 'wyrmrest-accord',
+    reputationVisible: false,
     showCharSelector: true,
 	posts: [
 		{title: "My very first Vue.js post", body: "lorem ipsum some test dimpsum"},
@@ -19,18 +25,18 @@ var landingpage = new Vue({
 
   },
   created: function() {
-  	axios.get('https://us.api.battle.net/wow/data/character/races?locale=en_US&apikey=xd6kggq2jgkb29d2db7t548j2dtp8r7d')
+  	axios.get(this.apiDataLocation + '/data/character/races?locale=en_US&apikey=xd6kggq2jgkb29d2db7t548j2dtp8r7d')
   		.then(response => {
   			this.charraces = response.data.races;
   		})
-	axios.get('https://us.api.battle.net/wow/data/character/classes?locale=en_US&apikey=xd6kggq2jgkb29d2db7t548j2dtp8r7d')
+	axios.get(this.apiDataLocation + '/data/character/classes?locale=en_US&apikey=xd6kggq2jgkb29d2db7t548j2dtp8r7d')
   		.then(response => {
   			this.charclasses = response.data.classes;
   		})
   },
   methods: {
 	fetchCharacter: function() {
-		var url = 'https://us.api.battle.net/wow/character/' +
+		var url = this.apiDataLocation + '/character/' +
 			this.realm + '/' + this.character + '?locale=en_US&apikey=xd6kggq2jgkb29d2db7t548j2dtp8r7d';
 		axios.get(url).then(response => {
 			this.apiData = response.data;
@@ -61,6 +67,41 @@ var landingpage = new Vue({
 	},
 	pickNewCharacter: function() {
 		this.showCharSelector = true;
+	},
+	showAchievements: function() {
+		this.achievesVisible = true;
+		this.battlePetsVisible = false;
+		this.mountsVisible = false;
+		this.profileVisible = false;
+		this.reputationVisible = false;
+	},
+	showBattlePets: function() {
+		this.achievesVisible = false;
+		this.battlePetsVisible = true;
+		this.mountsVisible = false;
+		this.profileVisible = false;
+		this.reputationVisible = false;
+	},
+	showMounts: function() {
+		this.achievesVisible = false;
+		this.battlePetsVisible = false;
+		this.mountsVisible = true;
+		this.profileVisible = false;
+		this.reputationVisible = false;
+	},
+	showProfile: function() {
+		this.achievesVisible = false;
+		this.battlePetsVisible = false;
+		this.mountsVisible = false;
+		this.profileVisible = true;
+		this.reputationVisible = false;
+	},
+	showReputation: function() {
+		this.achievesVisible = false;
+		this.battlePetsVisible = false;
+		this.mountsVisible = false;
+		this.profileVisible = false;
+		this.reputationVisible = true;
 	}
   }
 })  
